59ec266e1523c9569820e250f243bd4ff84966aa
CarlosMorette <carlos.morette@outlook.com>
2020-06-06 22:25:32 -0300

    Added project with NextJs

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout.js'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hellow, my name is Carlos Morette i'am from Brazil</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
